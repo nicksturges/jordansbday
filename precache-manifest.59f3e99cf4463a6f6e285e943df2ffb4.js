b967f23a98c01076d01a68f6fe9ff88c4aa94eba
Nick_Sturges <nicksturges@gmail.com>
2019-04-13 14:03:11 -0700

    Updates

---
+++ precache-manifest.59f3e99cf4463a6f6e285e943df2ffb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5edb3e9f4022618b6c34",
+    "url": "/jordansbday/static/css/main.2d305030.chunk.css"
+  },
+  {
+    "revision": "5edb3e9f4022618b6c34",
+    "url": "/jordansbday/static/js/main.4c6c1679.chunk.js"
+  },
+  {
+    "revision": "28e4141bd2df8ad81b26",
+    "url": "/jordansbday/static/js/runtime~main.601ac48c.js"
+  },
+  {
+    "revision": "8beccb564553040d13cc",
+    "url": "/jordansbday/static/css/2.2b6b6744.chunk.css"
+  },
+  {
+    "revision": "8beccb564553040d13cc",
+    "url": "/jordansbday/static/js/2.c2447a83.chunk.js"
+  },
+  {
+    "revision": "331d6b7cfdf0ac511b16f32ecdba36c0",
+    "url": "/jordansbday/index.html"
+  }
+];
